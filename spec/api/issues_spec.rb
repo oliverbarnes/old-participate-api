1484b4d5af1cc2396a5c94d84058d742c7906421
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2014-01-21 22:34:11 -0200

    Refactor issues_spec

--- spec/api/issues_spec.rb
+++ spec/api/issues_spec.rb
@@ -6,14 +6,14 @@ describe LiquidFeedback::Issues  do
   end
 
   describe 'GET /issues' do
+    subject { get '/issues' }
+
     it 'status 200' do
-      get '/issues'
-      last_response.status.should == 200
+      subject.status.should == 200
     end
 
     it 'empty collection' do
-      get '/issues'
-      JSON.parse( last_response.body ).should == []
+      JSON.parse( subject.body ).should == []
     end
 
     context 'when issues present' do
@@ -22,14 +22,11 @@ describe LiquidFeedback::Issues  do
       end
   
       it 'status 200' do
-        get '/issues'
-        last_response.status.should == 200
+        subject.status.should == 200
       end
 
       it "well-formed json collection" do
-        get '/issues'
-        last_response.status.should == 200
-        JSON.parse( last_response.body ).should == [{'name' => 'Kofi'}]
+        JSON.parse( subject.body ).should == [{'name' => 'Kofi'}]
       end
     end
   end
