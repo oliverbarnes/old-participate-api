a4b84d58d7dad4e46e96a3fec7f492fb94aea5f2
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2014-04-04 21:06:01 -0500

    Tweaks to factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,38 +2,19 @@ FactoryGirl.define do
   factory :issue do
     title "Public school in disrepair"
     description "My local school has missing lights, bathrooms that don't work"
-    # name "Kofi"
-    # area_id 1                     
-    # policy_id 1                   
-    # admin_notice ''
-    # state 'frozen'
-    # phase_finished Time.now              
-    # accepted Time.now                    
-    # half_frozen Time.now                 
-    # fully_frozen Time.now                
-    # closed Time.now                      
-    # cleaned Time.now                     
-    # admission_time_in_seconds 0   
-    # discussion_time_in_seconds  0  
-    # verification_time_in_seconds  0
-    # voting_time_in_seconds 0         
-    # snapshot Time.now                   
-    # latest_snapshot_event ''
-    # population 0     
-    # voter_count 0                
-    # status_quo_schulze_rank 0
   end
 
   factory :vote do
   end
 
   factory :member do
-    name "Alastair Reynolds"
+    #TODO: use faker at some point
+    sequence(:name) { |n| "Alastair Reynolds #{n}" }
   end
 
   factory :delegation do
     issue
     truster { FactoryGirl.create :member }
-    trustee { FactoryGirl.create :member, name: "Greg Egan" }
+    trustee { FactoryGirl.create :member }
   end
 end
