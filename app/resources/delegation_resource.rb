beef2958e9b50732bc3a797308a1d9464cd69d4e
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2015-11-08 18:46:38 -0200

    Add author/proposal filter to GET /delegations. Part of oliverbarnes/participate#133

--- app/resources/delegation_resource.rb
+++ app/resources/delegation_resource.rb
@@ -4,4 +4,20 @@ class DelegationResource < Base
   has_one :author
   has_one :proposal
   has_one :delegate
+
+  filters :proposal_id, :author_id
+
+  class << self
+    def apply_filter(records, filter, value, options = {})
+      value = fix_filter_value(value)
+      super(records, filter, value, options)
+    end
+
+    private
+
+      # Bug in JR wraps filter values in an array.
+      def fix_filter_value(value)
+        value.is_a?(Array) ? value.first : value
+      end
+  end
 end
